8b7fa2584135b6e67979e44d1ab9a9578b3cb851
ronny9313 <basu@BOSS.localdomain>
2018-11-16 19:13:52 +0530

    changed app js to angular

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn vue
+            Learn angular
           </a>
         </header>
       </div>
